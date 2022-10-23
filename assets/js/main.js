cc515d4011b1813801608fe68c2873594399ecf5
Bakumito <thomazfalbo@outlook.com>
2022-10-23 15:20:20 -0300

    Adicionado funcionalidade de criacao de linha na tabela, automaticamente ao clicar o botao de cadastro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 let tr = ``
 let idCounter = 0
 //Cadastrar Produto ao pressionar botao Cadastrar
-const botaoCadastro = document.querySelector('#botaoCadastro')
+const form = document.querySelector('#formulario')
 const tbody = document.querySelector('tbody')
 
 const createTableRow = (tbody, tr) => {
@@ -13,11 +13,21 @@ const createMsg = () => {
   const cells = document.getElementsByTagName('td')
 
   const ID = Number(rows.cells[0].innerHTML) + 1
+  const descricao = document.getElementById('descricaoProduto').value
+  const categoria = document.getElementById('categoriaProduto').value
+  const peso = Number(document.getElementById('pesoProduto').value)
+  const altura = Number(document.getElementById('alturaProduto').value)
+  const largura = Number(document.getElementById('larguraProduto').value)
+  const comprimento = Number(
+    document.getElementById('comprimentoProduto').value
+  )
+  const estoque = Number(document.getElementById('estoqueProduto').value)
+  const dataCadastro = document.getElementById('dataCadastroProduto').value
 
-  tr = `<tr class="linha"><td>${ID}</td><td>${2}</td><td>${3}</td><td>${4}</td><td>${5}</td><td>${6}</td><td>${7}</td><td>${8}</td><td>${9}</td></tr>`
+  tr = `<tr class="linha"><td>${ID}</td><td>${descricao}</td><td>${categoria}</td><td>${peso}</td><td>${altura}</td><td>${largura}</td><td>${comprimento}</td><td>${estoque}</td><td>${dataCadastro}</td></tr>`
 }
 
-botaoCadastro.addEventListener('click', e => {
+form.addEventListener('submit', e => {
   e.preventDefault()
   createMsg()
   createTableRow(tbody, tr)
