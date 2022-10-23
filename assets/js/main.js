a99d96ee5f0fb7ca0c73ba5818dc0d5fd9124ba3
Bakumito <thomazfalbo@outlook.com>
2022-10-23 15:50:22 -0300

    feito alguns ajustes default

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,27 +4,25 @@ let idCounter = 0
 const form = document.querySelector('#formulario')
 const tbody = document.querySelector('tbody')
 
-const createTableRow = (tbody, tr) => {
-  tbody.innerHTML += tr
-}
-
 const createMsg = () => {
   const rows = document.querySelectorAll('.linha')[idCounter]
   const cells = document.getElementsByTagName('td')
 
   const ID = Number(rows.cells[0].innerHTML) + 1
-  const descricao = document.getElementById('descricaoProduto').value
-  const categoria = document.getElementById('categoriaProduto').value
-  const peso = Number(document.getElementById('pesoProduto').value)
-  const altura = Number(document.getElementById('alturaProduto').value)
-  const largura = Number(document.getElementById('larguraProduto').value)
-  const comprimento = Number(
-    document.getElementById('comprimentoProduto').value
-  )
-  const estoque = Number(document.getElementById('estoqueProduto').value)
-  const dataCadastro = document.getElementById('dataCadastroProduto').value
+  const descricao = document.getElementById('descricao').value.toUpperCase()
+  const categoria = document.getElementById('categoria').value.toUpperCase()
+  const peso = Number(document.getElementById('peso').value)
+  const altura = Number(document.getElementById('altura').value)
+  const largura = Number(document.getElementById('largura').value)
+  const comprimento = Number(document.getElementById('comprimento').value)
+  const estoque = Number(document.getElementById('estoque').value)
+  const dataCadastro = document.getElementById('dataCadastro').value
 
   tr = `<tr class="linha"><td>${ID}</td><td>${descricao}</td><td>${categoria}</td><td>${peso}</td><td>${altura}</td><td>${largura}</td><td>${comprimento}</td><td>${estoque}</td><td>${dataCadastro}</td></tr>`
+}
+
+const createTableRow = (tbody, tr) => {
+  tbody.innerHTML += tr
 }
 
 form.addEventListener('submit', e => {
